32317fc17d8240b62f1f3bbb9d29a47412a1a190
lagrange.eth <lagrange@sorellacapital.com>
2024-02-03 19:05:33 -0500

    fixed other attr

--- redefined-derive/src/attributes/type_attr.rs
+++ redefined-derive/src/attributes/type_attr.rs
@@ -47,7 +47,6 @@ impl Parse for TypeAttribute {
                     let other_container_attrs = content
                         .parse_terminated(TokenStream::parse, Token![#])?
                         .into_iter()
-                        .skip(1)
                         .map(|stream| quote!(# #stream ))
                         .collect::<Vec<_>>();
 
